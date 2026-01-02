4b4717f45267df0942168f707a3c030853555629
That1L35 <randomguysaugwgwgwu@gmail.com>
2026-01-02 15:29:21 +0700

    Update game URLs in GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 local Games= {
-  [111510466699934] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Purgatory", -- Purgatory Lobby
-  [71184132329621] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Purgatory", -- Purgatory Game
+  [111510466699934] = "https://raw.githubusercontent.com/That1L35/Scripts/main/Purgatory.lua", -- Purgatory Lobby
+  [71184132329621] = "https://raw.githubusercontent.com/That1L35/Scripts/main/Purgatory.lua", -- Purgatory Game
 }
 return Games
