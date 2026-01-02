1f0d244ba88a86f0a6ab2ab5a8b1660c3578462d
That1L35 <randomguysaugwgwgwu@gmail.com>
2026-01-02 15:22:19 +0700

    Add new game entry to GameList

--- GameList.lua
+++ GameList.lua
@@ -1,2 +1,5 @@
 local Games= {
   [111510466699934] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Purgatory", -- Purgatory Lobby
+  [71184132329621] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Purgatory", -- Purgatory Game
+}
+return Games
