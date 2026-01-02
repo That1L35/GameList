daaa29a8be5fdc02e2bf27fb1c2b2495f8960ec2
That1L35 <randomguysaugwgwgwu@gmail.com>
2026-01-02 15:31:35 +0700

    Add new game scripts to GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,13 @@
 local Games= {
   [111510466699934] = "https://raw.githubusercontent.com/That1L35/Scripts/main/Purgatory.lua", -- Purgatory Lobby
   [71184132329621] = "https://raw.githubusercontent.com/That1L35/Scripts/main/Purgatory.lua", -- Purgatory Game
+  [117233475330049] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Mafia.lua", -- Mafia
+  [17218259625] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/WhosTheSpy.lua", -- Who's The Spy
+  [118384840573608] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/BeAbodybuilder", -- Be a Bodybuilder
+  [132620530679256] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/main/Pushtogether.lua", -- Lobby
+  [73367387755542] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/main/Pushtogether.lua", -- main game
+  [125886895122772] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/main/CubeGaem", -- Cube gaem
+  [126615659622875] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Anchored.lua", -- Anchored
+  [121677448523790] = "https://raw.githubusercontent.com/StupidityNoob/Scripts/refs/heads/main/Anchored.lua", -- The anchorage
 }
 return Games
